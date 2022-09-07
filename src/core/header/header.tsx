756d4febe98aa6b5fefa5991845980affeb9794c
Jorge Alia <jorgealia94@gmail.com>
2022-09-07 19:14:00 +0200

    Update menuMovil for responsive

--- src/core/header/header.tsx
+++ src/core/header/header.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { iState } from '../../store/store';
@@ -8,137 +7,53 @@ import './header.css';
 
 export function Header() {
     const user = useSelector((store: iState) => store.user);
-    const [clicked, setClicked] = useState(false);
-
-    function handleClick() {
-        setClicked(!clicked);
-    }
-
     function handleSubmit() {
         localStorage.clear();
         window.location.href = 'http://localhost:3000';
     }
     if (user.name !== '') {
-        if (clicked === false) {
-            return (
-                <>
-                    <div className="header">
-                        <Link to="">
-                            <img
-                                className="logo"
-                                src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/home%2Flogo.png?alt=media&token=580386eb-e5e2-4172-8bfa-26984e46905e"
-                                alt="Logo Aguado Pilates"
-                            />
-                        </Link>
+        return (
+            <>
+                <div className="header">
+                    <Link to="">
                         <img
-                            role="button"
-                            className="buttonMenu"
-                            src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/iconos%2Fbotonmenu.png?alt=media&token=705e2733-641e-4907-891b-33afd4606f7f"
-                            alt="Button menu"
-                            onClick={handleClick}
+                            className="logo"
+                            src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/home%2Flogo.png?alt=media&token=580386eb-e5e2-4172-8bfa-26984e46905e"
+                            alt="Logo Aguado Pilates"
                         />
-
-                        <MenuMovil clicked={clicked}></MenuMovil>
-                        <div className="menu">
-                            <Menu></Menu>
-                            <div className="wrapper_buttonLogout">
-                                <button
-                                    className="buttonLogout"
-                                    onClick={() => handleSubmit()}
-                                >
-                                    Salir
-                                </button>
-                            </div>
+                    </Link>
+                    <MenuMovil></MenuMovil>
+                    <div className="menu">
+                        <Menu></Menu>
+                        <div className="wrapper_buttonLogout">
+                            <button
+                                className="buttonLogout"
+                                onClick={() => handleSubmit()}
+                            >
+                                Salir
+                            </button>
                         </div>
                     </div>
-                </>
-            );
-        } else {
-            return (
-                <>
-                    <div className="header">
-                        <Link to="">
-                            <img
-                                className="logo"
-                                src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/home%2Flogo.png?alt=media&token=580386eb-e5e2-4172-8bfa-26984e46905e"
-                                alt="Logo Aguado Pilates"
-                            />
-                        </Link>
-                        <img
-                            role="button"
-                            className="buttonMenu"
-                            src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/iconos%2Fx.png?alt=media&token=d6669362-6cc9-4c90-9c05-eb7b7c9aadcf"
-                            alt="Button menu"
-                            onClick={handleClick}
-                        />
-
-                        <MenuMovil clicked={clicked}></MenuMovil>
-                        <div className="menu">
-                            <Menu></Menu>
-                            <div className="wrapper_buttonLogout">
-                                <button
-                                    className="buttonLogout"
-                                    onClick={() => handleSubmit()}
-                                >
-                                    Salir
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            );
-        }
+                </div>
+            </>
+        );
     } else {
-        if (clicked === false) {
-            return (
-                <>
-                    <div className="header">
-                        <Link to="">
-                            <img
-                                className="logo"
-                                src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/home%2Flogo.png?alt=media&token=580386eb-e5e2-4172-8bfa-26984e46905e"
-                                alt="Logo Aguado Pilates"
-                            />
-                        </Link>
+        return (
+            <>
+                <div className="header">
+                    <Link to="">
                         <img
-                            role="button"
-                            className="buttonMenu"
-                            src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/iconos%2Fbotonmenu.png?alt=media&token=705e2733-641e-4907-891b-33afd4606f7f"
-                            alt="Button menu"
-                            onClick={handleClick}
+                            className="logo"
+                            src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/home%2Flogo.png?alt=media&token=580386eb-e5e2-4172-8bfa-26984e46905e"
+                            alt="Logo Aguado Pilates"
                         />
-                        <MenuMovil clicked={clicked}></MenuMovil>
-                        <div className="menu">
-                            <Menu></Menu>
-                        </div>
+                    </Link>
+                    <MenuMovil></MenuMovil>
+                    <div className="menu">
+                        <Menu></Menu>
                     </div>
-                </>
-            );
-        } else {
-            return (
-                <>
-                    <div className="header">
-                        <Link to="">
-                            <img
-                                className="logo"
-                                src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/home%2Flogo.png?alt=media&token=580386eb-e5e2-4172-8bfa-26984e46905e"
-                                alt="Logo Aguado Pilates"
-                            />
-                        </Link>
-                        <img
-                            role="button"
-                            className="buttonMenu"
-                            src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/iconos%2Fx.png?alt=media&token=d6669362-6cc9-4c90-9c05-eb7b7c9aadcf"
-                            alt="Button menu"
-                            onClick={handleClick}
-                        />
-                        <MenuMovil clicked={clicked}></MenuMovil>
-                        <div className="menu">
-                            <Menu></Menu>
-                        </div>
-                    </div>
-                </>
-            );
-        }
+                </div>
+            </>
+        );
     }
 }
--- src/core/menu/menuMovil.tsx
+++ src/core/menu/menuMovil.tsx
@@ -1,19 +1,99 @@
-import { useSelector } from 'react-redux';
 import { Menu } from './menu';
-import { iState } from '../../store/store';
+
 import './menuMovil.css';
-export function MenuMovil({ clicked }: { clicked: boolean }) {
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { aMenuItems } from '../../models/menu.model';
+import AboutMePage from '../../pages/aboutMePage/aboutMePage';
+import ContactPage from '../../pages/contactPage/contactPage';
+import LoginPage from '../../pages/loginPage/loginPage';
+import MySelectionPage from '../../pages/mySelectionPage/mySelectionPage';
+import WorkoutsPage from '../../pages/workoutsPage/workoutsPage';
+import { iState } from '../../store/store';
+import './menu.css';
+export function MenuMovil() {
+    const optionsMenu: aMenuItems = [
+        {
+            path: 'disena',
+            label: 'Diseña',
+            page: <WorkoutsPage />,
+            title: 'Diseña',
+        },
+        {
+            path: 'entrena',
+            label: 'Entrena',
+            page: <MySelectionPage />,
+            title: 'Entrena',
+        },
+        {
+            path: 'sobremi',
+            label: 'Sobre mí',
+            page: <AboutMePage />,
+            title: 'Sobre mí',
+        },
+        {
+            path: 'contacto',
+            label: 'Contacto',
+            page: <ContactPage />,
+            title: 'Contacto',
+        },
+
+        {
+            path: 'login',
+            label: 'Login',
+            page: <LoginPage />,
+            title: 'Login',
+        },
+    ];
+
+    const [clicked, setClicked] = useState(false);
+    function handleClick() {
+        setClicked(!clicked);
+    }
+    console.log(clicked);
     const user = useSelector((store: iState) => store.user);
     function handleSubmit() {
         localStorage.clear();
         window.location.href = 'http://localhost:3000';
     }
+    if (user.name !== '') {
+        optionsMenu[4] = {
+            path: 'login',
+            label: 'Mi cuenta',
+            page: <LoginPage />,
+            title: 'Login',
+        };
+    }
     if (clicked === true) {
         if (user.name !== '') {
             return (
                 <>
+                    <img
+                        role="button"
+                        className="buttonMenu"
+                        src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/iconos%2Fx.png?alt=media&token=d6669362-6cc9-4c90-9c05-eb7b7c9aadcf"
+                        alt="Button menu"
+                        onClick={handleClick}
+                    />
                     <div className="menuMovil">
-                        <Menu></Menu>
+                        <nav>
+                            <ul className="menuList">
+                                {optionsMenu.map((item) => (
+                                    <li className="menu__item" key={item.label}>
+                                        <Link
+                                            to={item.path}
+                                            className="menu__link"
+                                            onClick={handleClick}
+                                        >
+                                            <h3 className="menu__title">
+                                                {item.label}
+                                            </h3>
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
                         <div className="wrapper_buttonLogout">
                             <button
                                 className="buttonLogout"
@@ -28,14 +108,61 @@ export function MenuMovil({ clicked }: { clicked: boolean }) {
         } else {
             return (
                 <>
+                    <img
+                        role="button"
+                        className="buttonMenu"
+                        src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/iconos%2Fx.png?alt=media&token=d6669362-6cc9-4c90-9c05-eb7b7c9aadcf"
+                        alt="Button menu"
+                        onClick={handleClick}
+                    />
                     <div className="menuMovil">
-                        <Menu></Menu>
+                        <nav>
+                            <ul className="menuList">
+                                {optionsMenu.map((item) => (
+                                    <li className="menu__item" key={item.label}>
+                                        <Link
+                                            to={item.path}
+                                            className="menu__link"
+                                            onClick={handleClick}
+                                        >
+                                            <h3 className="menu__title">
+                                                {item.label}
+                                            </h3>
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
                         <div className="wrapper_buttonLogout"></div>
                     </div>
                 </>
             );
         }
     } else {
-        return <></>;
+        if (user.name !== '') {
+            return (
+                <>
+                    <img
+                        role="button"
+                        className="buttonMenu"
+                        src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/iconos%2Fbotonmenu.png?alt=media&token=705e2733-641e-4907-891b-33afd4606f7f"
+                        alt="Button menu"
+                        onClick={handleClick}
+                    />
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <img
+                        role="button"
+                        className="buttonMenu"
+                        src="https://firebasestorage.googleapis.com/v0/b/aguado-pilates.appspot.com/o/iconos%2Fbotonmenu.png?alt=media&token=705e2733-641e-4907-891b-33afd4606f7f"
+                        alt="Button menu"
+                        onClick={handleClick}
+                    />
+                </>
+            );
+        }
     }
 }
